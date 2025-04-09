654a3f150a744d1bf45cec66941b6c7d13cce782
codehass <hassan.elouardy06@gmail.com>
2025-04-09 17:55:14 +0100

    add index and create methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,14 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path, notice: "Article was successfully created."
+    else
+      render :new
+    end
   end
 end
