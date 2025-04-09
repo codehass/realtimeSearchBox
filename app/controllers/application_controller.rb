0961d66fcbd7bd26af65bc69664b9f16acfbca42
codehass <hassan.elouardy06@gmail.com>
2025-04-09 17:54:51 +0100

    add helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  helper_method :current_visitor
+
+  def current_visitor
+    @current_visitor ||= Visitor.find_or_create_by(ip_address: request.remote_ip)
+  end
 end
