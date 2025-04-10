92467ef204722cf47c8eb347348e5765b1f92588
codehass <hassan.elouardy06@gmail.com>
2025-04-10 12:49:35 +0100

    add trending queries

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,8 @@
 class SearchesController < ApplicationController
   def index
     @searches = current_visitor.searches.order(created_at: :desc)
+
+    @trending_queries = Search.group(:query).order("count_id DESC").limit(3).count(:id)
   end
 
   private
